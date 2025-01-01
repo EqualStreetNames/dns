b908e1ee3f1285e0b7edab4e671902c8bba80779
Jonathan Beliën <jbelien@users.noreply.github.com>
2025-01-01 12:54:47 +0100

    Update dnsconfig.js

--- dnsconfig.js
+++ dnsconfig.js
@@ -113,6 +113,7 @@ D(
   CNAME("madrid", "equalstreetnames.github.io.", CF_PROXY_ON),
   CNAME("mainz", "equalstreetnames.github.io.", CF_PROXY_ON),
   CNAME("munich", "equalstreetnames.github.io.", CF_PROXY_ON),
+  CNAME("narva", "equalstreetnames.github.io.", CF_PROXY_ON),
   CNAME("oldenburg", "equalstreetnames.github.io.", CF_PROXY_ON),
   CNAME("paris", "equalstreetnames.github.io.", CF_PROXY_ON),
   CNAME("rastede", "equalstreetnames.github.io.", CF_PROXY_ON),
